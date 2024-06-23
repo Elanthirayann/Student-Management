1bcad3e95dff0c2f7040fda1b300e9cbce27065a
Neel-0808 <neelraj0808@gmail.com>
2024-06-23 22:51:55 +0530

    batman

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const Student = require('./models/student')
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/studentDB', { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect('mongodb://localhost:27017/studentDB');
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 });
+
+app.post('/register',async(req,res)=>{
+      try {
+    await Student.create(req.body);
+    res.status(201).send('User registered successfully');
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
